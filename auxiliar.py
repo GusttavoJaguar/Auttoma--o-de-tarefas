f665629a3fcbd9fc88774dce652977a03bd3a5f2
GusttavoJaguar <gutumendes8@gmail.com>
2025-11-14 11:39:30 -0300

    first commit

---
+++ auxiliar.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+
+time.sleep(5)
+print(pyautogui.position())
+
+# Botão login
+#Point(x=688, y=322)
+
+#import pandas
+
+#tabela = pandas.read_csv("Controle de estoque.csv")
+#print(tabela)
+
+#	id;	Nome;		Quantidade;	Preço;	Código de Barras;	Descrição;	Categoria
+#	0 	Camiseta;	50;			50;		564564546654;		Branca;		M         
+#	1 	Camiseta;	50;			50;		564564546654;		Preta;		G         
+#	2 	Camiseta;	50;			50;		564564546654;		Azul;		P         
+#	3 	Calça;		25;			100;	564564546654;		Jeans;		38         
+#	4 	Calça;		25;			100;	564564546654;		Preta;		42         
+#	5 	Calça;		25;			100;	564564546654;		Marrom;		50   
---
+++ main.py
@@ -0,0 +1,72 @@
+# PRINCIPAIS COMANDOS PYAUTOGUI							|
+# pyautogui.click -> clicar no mouse					|
+# pyautogui.write -> escrever texto						|
+# pyautogui.press -> apertar uma teclas					|
+# pyautogui.hotkey -> atalho(combinação de teclas)		|
+#-------------------------------------------------------
+
+# PASSO A PASSO 
+# Passo 1: Entrar no sistema da empresa
+	# localhost
+import pyautogui
+import time
+import pandas
+
+
+# Abrir o navegador
+pyautogui.PAUSE = 2
+
+pyautogui.press("win")
+pyautogui.write("firefox")
+pyautogui.press("enter")
+# Entrar no link 
+link = "http://localhost/ControleDeEstoque"
+pyautogui.write(link)
+pyautogui.press("enter")
+
+# Esperar o site carregar
+time.sleep(5)
+
+# Passo 2: Fazer login http://localhost/Controle%20de%20estoque/Controle%20de%20estoque/Controle%20de%20estoque/index.html
+pyautogui.click(x=688, y=322)
+pyautogui.click(x=532, y=311)
+pyautogui.click(x=532, y=311)
+pyautogui.hotkey("ctrl", "a")
+pyautogui.write("gusttavojaguar@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("1235813")
+pyautogui.press("enter")
+pyautogui.click(x=678, y=357)
+
+# Passo 3: Importar a base de dados de produtos
+
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+
+# Passo 4: Cadastrar 1 produto
+
+	pyautogui.click(x=340, y=376)
+
+	nome = tabela.loc[linha, "nome"]
+
+	# Preencher campos 
+	pyautogui.write(str(nome))
+	pyautogui.press("tab")
+	pyautogui.write(str(tabela.loc[linha, "quantidade"]))
+	pyautogui.press("tab")
+	pyautogui.write(str(tabela.loc[linha, "preco"]))
+	pyautogui.press("tab")
+	pyautogui.write(str(tabela.loc[linha,"cod"]))
+	pyautogui.press("tab")
+	pyautogui.write(str(tabela.loc[linha,"descricao"]))
+	pyautogui.press("tab")
+	pyautogui.write(str(tabela.loc[linha,"categoria"]))
+
+	pyautogui.press("tab")
+	pyautogui.press("enter")
+
+
+# Passo 5: Repetir o cadastro para todos os produtos
+
